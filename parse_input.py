05a7480962c1c89b9859918dff608e7bbc2824da
machnev <machnev@yandex-team.ru>
2019-02-09 16:33:56 +0300

    Убрал лишнее

--- parse_input.py
+++ parse_input.py
@@ -1,18 +1,15 @@
 lines = []
 
-with open("lines.txt") as input:
+with open("lines2.txt") as input:
     for line in input:
         line = line.replace("\r", "")
         line = line.replace("\n", "")
-        print(line)
         lines.append(line)
 
-with open("output.txt", "w") as output:
-    output.write("INPUT:text\tINPUT:part\n")
+with open("output2.txt", "w") as output:
     for line in lines:
         tokens = line.split(" ")
         for token in tokens:
-            l = line + "\t" + token + "\n"
-            output.write(l)
+            output.write(token + "\n")
   
 
