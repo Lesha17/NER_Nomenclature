be47c53d38de995d3c3a444b04757bdb65cfb1d5
machnev <machnev@yandex-team.ru>
2019-03-07 13:14:37 +0300

    Использую разные split_pattern для словаря

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ from nomenclature_parser.fuzzy_logic import fuzzy_logic
 from nomenclature_parser.machine_learning_dictionary import create_word_dictionary
 from nomenclature_parser.pattern_parser import parse_nomenclature
 
-split_pattern = ' |\n|;|,|\\(|\\)'
 nomenclature = pd.read_excel("nomenclature.xlsx")
 nomenclature = nomenclature.append(pd.read_excel("nomenclature2.xlsx"))
 nomenclature_patterns = pd.read_excel("nomenclature_patterns.xlsx")
 
-word_dictionary = create_word_dictionary(nomenclature, nomenclature_patterns, split_pattern)
+main_split_pattern = ' |\n|;|,|\\(|\\)'
+other_split_patters = [' |-|\n|;|,|\\(|\\)', # with '-'
+' |\n|;|,', # without braces
+' |-|\n|;|,', # with '-' without braces
+' |\n|;|\\(|\\)', # without comma
+' |-|\n|;|\\(|\\)', # with '-' without comma
+' |\n|;', # without braces without comma
+' |-|\n|;' # with '-' without braces without comma
+]
+
+word_dictionary = create_word_dictionary(nomenclature, nomenclature_patterns, main_split_pattern)
+for split_pattern in other_split_patters:
+	word_dictionary = word_dictionary.union(create_word_dictionary(nomenclature, nomenclature_patterns, split_pattern))
 dataframes, word2pc, word2predict, cntr = fuzzy_logic(word_dictionary)
 parsed_nomenclature = parse_nomenclature(split_pattern, word2pc, word2predict, cntr)
 
