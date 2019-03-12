b08a3170b74d721b653c2aae2bcc05901710b0ea
xomielanne <demidafonichev@gmail.com>
2019-03-12 15:06:03 +0300

    Add changing status label ans progress bar during parsing

--- main.py
+++ main.py
@@ -1,38 +1,5 @@
-import pandas as pd
-import os
-
-from nomenclature_parser.machine_learning_dictionary import create_word_dictionary, save_dict, load_dict
-from nomenclature_parser.pattern_parser import parse_nomenclature
 from ui.NomenclatureParserUI import NomenclatureParserUI
 
-nomenclature_parser_ui = NomenclatureParserUI()
-nomenclature_parser_ui.mainloop()
-print("here 2")
 
-split_pattern = ' |\n|;|,|\\(|\\)'
-nomenclature = pd.read_excel("nomenclature.xlsx")
-nomenclature = nomenclature.append(pd.read_excel("nomenclature2.xlsx"), ignore_index=True)
-nomenclature_patterns = pd.read_excel("nomenclature_patterns.xlsx")
-
-main_split_pattern = ' |\n|;|\\(|\\)'
-split_patterns = [main_split_pattern]
-
-dictFile = 'dict.txt'
-
-if os.path.isfile(dictFile):
-    word_dictionary = load_dict(dictFile)
-    print("Loaded dictionary from {}".format(dictFile))
-else:
-    print("Creating dictionary")
-    word_dictionary = set()
-    for split_pattern in split_patterns:
-        print("Splitting using pattern {}".format(split_pattern))
-        result_dict = create_word_dictionary(nomenclature, nomenclature_patterns, split_pattern)
-        print("Size of dict: {}".format(len(result_dict)))
-        word_dictionary = word_dictionary.union(result_dict)
-    print("Dictionary size: {}".format(len(word_dictionary)))
-    save_dict(word_dictionary, dictFile)
-
-parsed_nomenclature = parse_nomenclature(split_patterns, list(word_dictionary))
-
-parsed_nomenclature.to_excel("parsed_nomenclature.xlsx")
+if __name__ == "__main__":
+    NomenclatureParserUI().mainloop()
--- ui/NomenclatureParserUI.py
+++ ui/NomenclatureParserUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.filedialog import *
+from tkinter.ttk import *
 
 import pandas as pd
 
@@ -11,7 +12,7 @@ class NomenclatureParserUI(Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.winfo_toplevel().title("NomenclatureParser")
+        self.title("NomenclatureParser")
 
         Label(self, text="Номенклатура") \
             .grid(row=0, pady=4, padx=10, sticky=W)
@@ -37,46 +38,64 @@ class NomenclatureParserUI(Tk):
         Button(self, text="Распознать", command=self.start_parsing) \
             .grid(row=3, column=1, ipady=4, ipadx=16, pady=8, padx=10, sticky=E)
 
+        # self.status = Label(self, text="")
+        self._status = StringVar(self)
+        self.status = Label(self, textvariable=self._status)
+        self.status.grid(row=4, columnspan=3)
+
+        self._progress_bar_value = IntVar()
+        self._progress_bar_value.set(0)
+        self._progress_bar_max_value = IntVar()
+        self._progress_bar_max_value.set(0)
+
+        self.progress_bar = Progressbar(self, variable=self._progress_bar_value, maximum=0)
+        self.progress_bar.grid(row=5, columnspan=3)
+
     def start_parsing(self):
-        print("here")
         if self._nomenclature_file_path is not None and self._nomenclature_patterns_file_path is not None:
             nomenclature = pd.read_excel(self._nomenclature_file_path)
             nomenclature_patterns = pd.read_excel(self._nomenclature_patterns_file_path)
-            # nomenclature = nomenclature.append(pd.read_excel("nomenclature2.xlsx"), ignore_index=True)
 
-            main_split_pattern = ' |\n|;|,|\\(|\\)'
-            other_split_patters = [
-                ' |-|\n|;|,|\\(|\\)',  # with '-'
-                ' |\n|;|,',  # without braces
-                ' |-|\n|;|,',  # with '-' without braces
-                ' |\n|;|\\(|\\)',  # without comma
-                ' |-|\n|;|\\(|\\)',  # with '-' without comma
-                ' |\n|;',  # without braces without comma
-                ' |-|\n|;'  # with '-' without braces without comma
-            ]
-
-            split_patterns = other_split_patters
-            split_patterns.append(main_split_pattern)
+            main_split_pattern = ' |\n|;|\\(|\\)'
+            split_patterns = [main_split_pattern]
 
             dictFile = 'dict.txt'
 
             if os.path.isfile(dictFile):
+                self.progress_bar.configure(maximum=3)
+                self._set_status("Загружаем словарь данных")
                 word_dictionary = load_dict(dictFile)
+                self._increase_progress_bar()
+
                 print("Loaded dictionary from {}".format(dictFile))
             else:
+                self.progress_bar.configure(maximum=4)
                 print("Creating dictionary")
                 word_dictionary = set()
+                self._set_status("Создаем словарь данных")
                 for split_pattern in split_patterns:
                     print("Splitting using pattern {}".format(split_pattern))
+
                     result_dict = create_word_dictionary(nomenclature, nomenclature_patterns, split_pattern)
+
                     print("Size of dict: {}".format(len(result_dict)))
                     word_dictionary = word_dictionary.union(result_dict)
+                self._increase_progress_bar()
                 print("Dictionary size: {}".format(len(word_dictionary)))
+
+                self._set_status("Сохраняем словарь данных")
                 save_dict(word_dictionary, dictFile)
+                self._increase_progress_bar()
 
+            self._set_status("Распознаем номенклатуру")
             parsed_nomenclature = parse_nomenclature(split_patterns, list(word_dictionary))
+            self._increase_progress_bar()
 
+            self._set_status("Сохраняем номенклатуру")
             parsed_nomenclature.to_excel("parsed_nomenclature.xlsx")
+            self._increase_progress_bar()
+
+            self._set_status("Готово")
 
     def _get_nomenclature_file_path(self):
         self._nomenclature_file_path = self._get_file()
@@ -88,6 +107,14 @@ class NomenclatureParserUI(Tk):
     def _get_file():
         file_path = askopenfilename(filetypes=[("Excel", "*.xlsx")])
         return file_path
+
+    def _set_status(self, status):
+        self._status.set(status)
+        self.update()
+
+    def _increase_progress_bar(self):
+        self._progress_bar_value.set(self._progress_bar_value.get() + 1)
+        self.update()
 
     _nomenclature_file_path = "./nomenclature.xlsx"
     _nomenclature_patterns_file_path = "./nomenclature_patterns.xlsx"
